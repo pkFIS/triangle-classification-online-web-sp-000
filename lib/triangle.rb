e730086abd97dd9db36f51b355d8b9d0ac44b0f3
Paul K <pkfis@outlook.com>
2019-05-10 06:32:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
   # write code here
+
+  def initialize(side1, side2, side3)
+
+
+
+  end
+
+  def kind
+
+  end
+
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
 end
